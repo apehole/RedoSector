4da93363f541614afd7a0836560201f213daf7c9
apehole <apehole@163.com>
2020-12-29 10:00:05 +0800

    make redo cmd

--- make_redo_cmd/make_redo_cmd.py
+++ make_redo_cmd/make_redo_cmd.py
@@ -1,25 +1,23 @@
-import configparser
+
 import requests
-import base64
-import os
-import shutil
-import re
-from time import gmtime, strftime
 
 
-def get_miner_post():
-    config = configparser.ConfigParser()
-    config.read("token.ini", encoding="utf-8")
-    api = config.get('POST', 'API_MINER')
-    token = config.get('POST', 'TOKEN_MINER')
-    url = "http://" + eval(api) + "/rpc/v0"
-    return url, eval(token)
+miner_url = ""
 
 
-def get_sectors_status(miner_url, miner_token, number):
+# def get_miner_post():
+#     config = configparser.ConfigParser()
+#     config.read("token.ini", encoding="utf-8")
+#     api = config.get('POST', 'API_MINER')
+#     token = config.get('POST', 'TOKEN_MINER')
+#     url = "http://" + eval(api) + "/rpc/v0"
+#     return url, eval(token)
+
+
+def get_sectors_status(miner_url, number):
     headers = {'Accept': 'application/json',
                'Content-Type': 'application/json',
-               'Authorization': 'Bearer ' + miner_token}
+               }
     params = {
         "jsonrpc": "2.0",
         "method": "Filecoin.SectorsStatus",
@@ -36,27 +34,33 @@ def get_sectors_status(miner_url, miner_token, number):
         result = r.json()["result"]
         return result
 
+
 def print():
     # file1=open("/home/fqw/20201226faultsectors_sorted", "r+")
-    file2=open("/home/fqw/sectorCmd.txt", "w+")
-    miner_url, miner_token = get_miner_post()
-    i=1
-    with open('/home/fqw/20201226faultsectors_sorted', 'r') as f:
+    file2 = open("/Users/iron_huang/IdeaProjects/RedoSector/make_redo_cmd/redo_cmd.txt", "w+")
+    # miner_url, miner_token = get_miner_post()
+    with open('/Users/iron_huang/IdeaProjects/RedoSector/make_redo_cmd/numbers.txt', 'r') as f:
         for line in f:
-            number=int(line)
-            sectorStatus=get_sectors_status(miner_url, miner_token, number)
+            number = int(line)
+            sectorStatus = get_sectors_status(miner_url, number)
             if not sectorStatus is None:
-                sealProofType=sectorStatus["SealProof"]
-                ticket=sectorStatus["Ticket"]["Value"]
-                if number >=0:
-                    if (i%2)==0:
-                        cmd="lotus-scheduler add-redo-task --sealProofType "+str(sealProofType)+" --minerID "+"54267"+" --sectorNumber "+str(number)+" --ticketBase64 "+str(ticket)+" --minerSealedDir /mnt/data/data_nfs4/sealed --minerCacheDir /mnt/data/data_nfs4/cache"
+                sealProofType = sectorStatus["SealProof"]
+                ticket = sectorStatus["Ticket"]["Value"]
+                if number >= 0:
+                    if (number % 2) == 0:
+                        cmd = "lotus-scheduler add-redo-task --sealProofType " + str(
+                            sealProofType) + " --minerID " + "54267" + " --sectorNumber " + str(
+                            number) + " --ticketBase64 " + str(
+                            ticket) + " --minerSealedDir /mnt/data/data_nfs4/sealed --minerCacheDir /mnt/data/data_nfs4/cache"
                     else:
-                        cmd="lotus-scheduler add-redo-task --sealProofType "+str(sealProofType)+" --minerID "+"54267"+" --sectorNumber "+str(number)+" --ticketBase64 "+str(ticket)+" --minerSealedDir /mnt/data/data_nfs5/sealed --minerCacheDir /mnt/data/data_nfs5/cache"
+                        cmd = "lotus-scheduler add-redo-task --sealProofType " + str(
+                            sealProofType) + " --minerID " + "54267" + " --sectorNumber " + str(
+                            number) + " --ticketBase64 " + str(
+                            ticket) + " --minerSealedDir /mnt/data/data_nfs5/sealed --minerCacheDir /mnt/data/data_nfs5/cache"
                     file2.writelines(cmd + '\n')
-                    i+=1
     f.close()
     file2.close()
+
 
 if __name__ == '__main__':
     print()
